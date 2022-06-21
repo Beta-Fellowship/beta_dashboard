ce8f793bc039d01f8f7c002bad593f7eb9f12bdf
pette1999 <haichen@chapman.edu>
2022-06-20 21:21:25 -0700

    have the status ready and session stored in the database

--- src/routes/auth/index.ts
+++ src/routes/auth/index.ts
@@ -11,4 +11,8 @@ router.get('/discord/redirect', passport.authenticate('discord'), (req, res) => 
   res.send({msg: 'success'});
 });
 
+router.get('/status', (req, res) => {
+  return req.user ? res.send(req.user) : res.status(401).send({ msg: 'Unauthorized!' });
+});
+
 export default router;
