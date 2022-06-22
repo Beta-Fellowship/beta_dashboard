9db151a7c3e38491ede26020d019216f40284a50
pette1999 <haichen@chapman.edu>
2022-06-22 02:01:44 -0700

    update the menu bar, change the logo and profile pic, change the layout and the selected effect

--- beta-dashboard-client/src/components/Navbar.tsx
+++ beta-dashboard-client/src/components/Navbar.tsx
@@ -4,25 +4,33 @@ import Image from 'next/image'
 import React from "react";
 
 import beta_logo from '../../public/beta.png'
+import profile from '../../public/profile2.png'
 
-const NavBarItem = ({ title, classprops }: { title:any, classprops:any }) => (
-  <li className={`mx-4 py-1 px-5 cursor-pointer border-solid border-white border-2 rounded-md text-xs ${classprops}`}>{title}</li>
+const NavBarItem = ({ title, classprops, extraprops }: { title: any, classprops: any, extraprops:any }) => (
+  <div className={`${extraprops}`}>
+    <li className={`py-2 px-7 bg-[#0B0B0B] cursor-pointer rounded`}>
+      <p className={`${classprops} font-['Poppins'] text-xs`}>{title}</p>
+    </li>
+  </div>
+  
 );
 
-const Navbar = () => {
+const Navbar = ({page}:{page:string}) => {
   const [toggleMenu, setToggleMenu] = React.useState(false);
 
   return (
     <nav className="w-full flex md:justify-center justify-between items-center p-2 bg-[#0B0B0B] pt-8">
       <div className="md:flex-[0.9] flex-initial justify-center items-center">
-        <Image src={beta_logo} alt="Beta Logo" className="w-32 cursor-pointer" />
+        <Image src={beta_logo} alt="Beta Logo" width={100} height={60} className="object-scale-down cursor-pointer" />
       </div>
       <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial">
         {["Dashboard", "Earn Points"].map((item, index) => (
-          <NavBarItem key={item + index} title={item} />
+          item == page ? <NavBarItem key={item + index} title={item} extraprops="py-0.5 px-0.5 mx-4 rounded-md bg-gradient-to-r from-[#FFD42B] to-[#53A548]" classprops="font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-[#FFD42B] to-[#53A548]" /> : <NavBarItem key={item + index} title={item} extraprops="" classprops="" />
         ))}
-        <li className="bg-slate-300 py-4 px-4 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]"></li>
-        <li className="bg-[#707070] py-1 px-7 rounded-md cursor-pointer">Angielyz</li>
+        <li className="flex flex-row justify-start mx-4 cursor-pointer space-x-2">
+          <Image src={profile} alt="Beta Logo" width={28} height={28} className="object-scale-down cursor-pointer" />
+          <p className="bg-[#707070] bg-opacity-40 py-1 px-7 rounded-md cursor-pointer">Angielyz</p>
+        </li>
       </ul>
       <div className="flex relative">
         {!toggleMenu && (
@@ -38,7 +46,7 @@ const Navbar = () => {
           >
             <li className="text-xl w-full my-2"><AiOutlineClose onClick={() => setToggleMenu(false)} /></li>
             {["Dashboard", "Earn Points"].map(
-              (item, index) => <NavBarItem key={item + index} title={item} classprops="my-2 text-lg" />,
+              (item, index) => <NavBarItem key={item + index} title={item} extraprops="" classprops="my-2 text-lg" />,
             )}
           </ul>
         )}
