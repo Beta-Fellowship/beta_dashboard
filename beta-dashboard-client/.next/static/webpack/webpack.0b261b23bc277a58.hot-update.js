bebe5e53910bcc3706a63f4bb5136d3351144acc
pette1999 <haichen@chapman.edu>
2022-06-22 05:00:25 -0700

    added page routing

--- beta-dashboard-client/.next/static/webpack/webpack.0b261b23bc277a58.hot-update.js
+++ beta-dashboard-client/.next/static/webpack/webpack.0b261b23bc277a58.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "5aec4fe899f84c74"; }
+/******/ 	__webpack_require__.h = function() { return "310cf8d3abe670d2"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
